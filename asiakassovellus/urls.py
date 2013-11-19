74ec97b4342556c77f4b58dfdfdea41591a9d061
matiasturunen <matias@skyred.fi>
2013-11-19 19:49:52 +0200

    translate to english

--- asiakassovellus/urls.py
+++ asiakassovellus/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^hallinta/', include('hallinta.urls', namespace='hallinta'))
 )

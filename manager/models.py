6342cf29b52183f7560c6d551d7c2c255c013d12
matiasturunen <matias@skyred.fi>
2013-12-10 14:47:06 +0200

    add model based forms

--- manager/models.py
+++ manager/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.forms import ModelForm
+from django.forms.widgets import TextInput, Textarea, Select
 
 class Customer(models.Model):
     name = models.CharField(max_length=200)
@@ -19,4 +21,42 @@ class Contact(models.Model):
     
     def __unicode__(self):
         return self.lastname + ' ' + self.name
-    
+
+class CustomerForm(ModelForm):
+    class Meta:
+        model = Customer
+        fields = ('name', 'address', 'extrainfo')
+        labels = {
+            'name': ('Name'),
+            'address': ('Address'),
+            'extrainfo': ('Additional details'),
+        }
+        widgets = {
+            'name': TextInput(attrs={'class': 'form-control'}),
+            'address': TextInput(attrs={'class': 'form-control'}),
+            'extrainfo': Textarea(attrs={'class': 'form-control'}),
+        }
+        
+        
+class ContactForm(ModelForm):
+    class Meta:
+        model = Contact
+        fields = ('customer', 'name', 'lastname', 'job', 'phone', 'email')
+        labels = {
+            'customer': ('Customer'),
+            'name': ('First name'),
+            'lastname': ('Last name'),
+            'job': ('Job description'),
+            'phone': ('Phone'),
+            'email': ('E-mail'),
+            
+        }
+        widgets = {
+            'customer': Select(attrs={'class': 'form-control'}),
+            'name': TextInput(attrs={'class': 'form-control'}),
+            'lastname': TextInput(attrs={'class': 'form-control'}),
+            'job': TextInput(attrs={'class': 'form-control'}),
+            'phone': TextInput(attrs={'class': 'form-control'}),
+            'email': TextInput(attrs={'class': 'form-control'}),
+        }
+        
